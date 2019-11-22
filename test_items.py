c64bdb743c5bf96c2ce2bd4fb2bb883f238967c0
Olga Emelyanova <emelyanova@t8.ru>
2019-11-22 14:36:12 +0300

    Refactor

--- test_items.py
+++ test_items.py
@@ -4,7 +4,7 @@ import time
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
-def test_basket(browser):
+def test_button_basket(browser):
     browser.get(link)
     time.sleep(5)
     assert browser.find_element_by_css_selector('button.btn-add-to-basket')
